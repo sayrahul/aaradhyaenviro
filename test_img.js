be30b3ed0f7e6f7897fa319b6ce29bce572d89d1
Rahul Jadhav <sayrahul>
2026-03-07 08:38:40 +0530

    Add legal pages, long-form policies, sitemap page, and standardize legal links/site structure

--- test_img.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('d:/My Web Sites/aaradhya 2 - V0/products.html', 'utf8');
-const match = html.match(/<img[^>]*Fire-Sprinkler-Set[^>]*>/);
-console.log(match ? match[0] : 'Not found');
